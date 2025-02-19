8fe80fed95bfff1ddfccf67a1e2a26a5f32690e4
Htin Wana <htinwana.dev@gmail.com>
2025-02-19 13:38:52 +0630

    fix: change server port to 3001

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ export class App {
   constructor(routes: Routes[]) {
     this.app = express();
     this.env = NODE_ENV || 'development';
-    this.port = PORT || 3000;
+    this.port = 3001;
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
